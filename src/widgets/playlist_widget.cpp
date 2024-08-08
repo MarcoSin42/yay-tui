9eeee7f1ce401ff7ae51b9be1ed3444289eaa297
Marco S <marcosin918@gmail.com>
2024-08-07 19:31:59 -0600

    refactor: Allow for adjustable spacing

--- src/widgets/playlist_widget.cpp
+++ src/widgets/playlist_widget.cpp
@@ -15,10 +15,19 @@
 using namespace ftxui;
 using namespace std;
 
+namespace {
+
 Component Window(std::string title, Component component) {
   return Renderer(component, [component, title] {  //
     return window(text(title), component->Render()) | flex;
   });
+}
+
+const int TITLE_SPACING = 40;
+const int ALBUM_SPACING = 25;
+const int ARTIST_SPACING = 25;
+const int RUNTIME_SPACING = 6;
+
 }
 
 namespace widgets {
@@ -26,13 +35,21 @@ namespace widgets {
         Components snames;
         for (track_info & track : *tracks) {
             snames.push_back(MenuEntry(format(
-                "{:<25} | {:<25} | {:<25} | {:^8}",
-                track.title, track.album, track.artist, format("{}:{:02}", track.mins, track.secs)
+                "{0:{1}.{1}} | {2:{3}.{3}} | {4:{5}.{5}} | {6:{7}.{7}}",
+                track.title, TITLE_SPACING,
+                track.album, ALBUM_SPACING,
+                track.artist, ARTIST_SPACING,
+                format("{}:{:02}", track.mins, track.secs), RUNTIME_SPACING
             )));
         }
 
         Component menu = Window(
-            format("  {:^25} | {:^25} | {:^25} | {:^8}", "Song", "Album","Artist", "Runtime"),
+            format("  {0:^{1}} | {2:^{3}} | {4:^{5}} | {6:^{7}}", 
+                "Song", TITLE_SPACING,
+                "Album", ALBUM_SPACING,
+                "Artist", ARTIST_SPACING,
+                "Runtime", RUNTIME_SPACING
+            ),
             Container::Vertical(snames, selected)
         );
         return menu;
